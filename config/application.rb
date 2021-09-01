60b7bdd9054d03d953164672e1e8b7ce46a8957d
Brayan.Granada <brayan.granada.kommit@gmail.com>
2021-09-01 15:40:16 -0500

    To add auth API

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,19 @@ module TodosApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
     config.generators do |g|
       g.factory_bot suffix: 'factory'
+    end
+
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+        headers: :any,
+        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+        methods: [:get, :post, :options, :delete, :put, :patch]
+      end
     end
   end
 end
